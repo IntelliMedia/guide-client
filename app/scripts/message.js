8f38cb5e700677745e94883e45508b0e866905fa
Robert Taylor <rgtaylor@ncsu.edu>
2018-02-26 19:18:21 -0500

    Resolved #15 by implementing a new tab that allows client to send raw JSON messages to the server.

---
+++ app/scripts/message.js
@@ -0,0 +1,41 @@
+initialize();
+
+function initialize() {
+    console.info("Initialize message send UI");
+
+    $('.sendMessageButton').each(function () {
+        $(this).on("click", sendMessage);
+    });
+}
+
+function sendMessage() {
+
+    try
+    {
+    console.info("SendMessage");
+
+    var msgText = $('#message-area').val();
+    var msg = JSON.parse(msgText);
+
+    SendGuideEvent(
+        msg.actor,
+        msg.action,
+        msg.target,
+        msg.context);
+    } catch(e) {
+        showPopup(
+            'error',
+            "Unable to send message",
+            e.message);        
+    }
+}
+
+function getStudentId() {
+    var studentId = $('#studentIdInput').val();
+    if (!studentId) {
+        studentId = randomStudentId();
+        $('#studentIdInput').val(studentId);
+    }
+
+    return studentId;
+}
