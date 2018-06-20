d2b4d135918dd0dc0793764a04abece818d6f29e
Robert Taylor <rgtaylor@ncsu.edu>
2018-06-20 16:22:39 -0400

    Updated version to 3.4.0

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,7 +6,7 @@
  * Constants
  */
 
-const ClientVersion = '3.1.0';
+const ClientVersion = '3.4.0';
 const GuideSocketioNamespace = 'guide-protocol';
 
 const imageUrlBase = 'https://geniverse-resources.concord.org/resources/drakes/images/';
